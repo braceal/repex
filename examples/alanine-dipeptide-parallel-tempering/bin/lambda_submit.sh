f302b9ae2ba344301fa15ea680a3cd5e3f756bfa
dgorgun <defnegorgund@gmail.com>
2021-06-17 12:29:08 -0700

    lambda

--- examples/alanine-dipeptide-parallel-tempering/bin/lambda_submit.sh
+++ examples/alanine-dipeptide-parallel-tempering/bin/lambda_submit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#SBATCH -N 1 
 #SBATCH --exclusive
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH -A lambda
 #SBATCH -J repex
 #SBATCH -t 00:10:00
 
 set -x
 rankspernode=2 #4
-totalranks=$(( ${SLURM_NNODES} * ${rankspernode} ))
-
+totalranks=$(( ${SLURM_NNODES}*${rankspernode} ))
+echo $rankerspernode
 # Run repex
 srun -l -u --wait=30 -N ${SLURM_NNODES} -n ${totalranks} -c $(( 40 / ${rankspernode} )) --cpu_bind=cores --mpi=pmi2 ./bin/lambda_run.sh
